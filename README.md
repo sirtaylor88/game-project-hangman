4bf6e413ba4f5ae6119a2352ec0d167747c71c22
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2022-03-07 16:31:03 +0100

    Added game plan.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # game-project-hangman
+
+The player has to guess the full word by suggesting letters within a certain number of guesses.
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	// Derive a word we have to guess
+	// Printing game state
+	//   *  Print word you're guessing
+	//   *  Print hangman state
+	// Read user input
+	//   *  Validate it (e.g. only letters)
+	// Determine if the letter is a correct guess or not
+	//   *  If correct, update the guessed letter
+	//   *  If incorrect, update the hangman state
+	// If word is guessed, game over, you win
+	// If hangman is complete, game over, you lose
+}
